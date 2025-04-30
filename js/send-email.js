e841277dd0f29b2c12f1d3e27761cf372110e60e
michelcun <michel.meneses@cun.edu.co>
2025-04-29 23:58:20 -0500

    Fix: Reset form after send

--- js/send-email.js
+++ js/send-email.js
@@ -5,12 +5,13 @@ document.querySelector("form").addEventListener("submit", function (e) {
     e.preventDefault();
 
     emailjs.sendForm(
-        "service_g0uo7j4",         // ✅ Nuevo Service ID
+        "service_g0uo7j4",         // Nuevo Service ID
         "template_4fzxkog",        // Tu ID de plantilla
         this,                      // El formulario
            )
         .then(function () {
             alert("✅ Tu mensaje ha sido enviado correctamente.");
+            document.querySelector("form").reset(); //Limpia el formulario
         }, function (error) {
             alert("❌ Ocurrió un error al enviar el mensaje.");
             console.error("Error:", error);
